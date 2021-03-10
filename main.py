35c6ad25f14d2e44579c66632caf147cd2ab3fd0
Matias Brula <mbrula@itba.edu.ar>
2021-03-10 09:14:52 -0300

    reformat main code to match python syntax standards

--- main.py
+++ main.py
@@ -21,27 +21,29 @@ y = 0
 for line in file:
     x = 0
     for character in line:
-        if character == '\n': continue
-        x+=1
+        if character == '\n':
+            continue
+        x += 1
         if character == '#':
-            staticMap[obj.Point(x,y)] = mapFun.Element.Wall
+            staticMap[obj.Point(x, y)] = mapFun.Element.Wall
         elif character == '.':
-            staticMap[obj.Point(x,y)] = mapFun.Element.Goal
+            staticMap[obj.Point(x, y)] = mapFun.Element.Goal
         elif character == '$':
-            boxesInit.append(obj.Point(x,y))
-        elif (character == '@'):
-            playerInit = obj.Point(x,y)
-        elif (character == '*'): 
-            staticMap[obj.Point(x,y)] = mapFun.Element.Goal
-            boxesInit.append(obj.Point(x,y))
-        elif (character == '+'): 
-            staticMap[obj.Point(x,y)] = mapFun.Element.Goal
-            playerInit = obj.Point(x,y)
-        elif (character == ' '): continue
-    y+=1
+            boxesInit.append(obj.Point(x, y))
+        elif character == '@':
+            playerInit = obj.Point(x, y)
+        elif character == '*':
+            staticMap[obj.Point(x, y)] = mapFun.Element.Goal
+            boxesInit.append(obj.Point(x, y))
+        elif character == '+':
+            staticMap[obj.Point(x, y)] = mapFun.Element.Goal
+            playerInit = obj.Point(x, y)
+        elif character == ' ':
+            continue
+    y += 1
 
-# TODO: Metemos alguna validacion de mapa simple?
-#       Que no haya 2 playerInit, que haya # boxes <= # goals
+# TODO: Create simple map validation?
+#       No 2 playerInit, # boxes <= # goals
 initNode = obj.Node(playerInit, 0, boxesInit)
 
 end_time = time.time()
@@ -49,25 +51,26 @@ print(f'Took {end_time - startTime} to read Config and Map')
 startTime = end_time
 
 algo = mapFun.BFS(staticMap, initNode)
-while (not algo.isAlgorithmOver()):
+while not algo.isAlgorithmOver():
     curNode = algo.iterate()
     # print(f'{algo.nodeCollection}\n')
 
 end_time = time.time()
 print(f'Took {end_time - startTime} to run algorithm')
 
-# TODO: Imprimir parametros de busqueda
+# TODO: Print search params
 
 # TODO: Move printing to function/other place, add return in failure instead of if/else
-if (not algo.winnerNode):
+if not algo.winnerNode:
     # Solution not found
     print("Failure!")
 else:
     # Solution found
     print("Success!")
-    print(f'Depth: {algo.winnerNode.depth}; Expanded nodes: {algo.expandedCount}; Border nodes: {algo.getBorderCount()}')
+    print(
+        f'Depth: {algo.winnerNode.depth}; Expanded nodes: {algo.expandedCount}; Border nodes: {algo.getBorderCount()}')
     roadStack = algo.getWinningRoadStack()
 
     # TODO: Print map instead of nodes
-    while (roadStack):
+    while roadStack:
         print(f'{roadStack.pop()}\n')
