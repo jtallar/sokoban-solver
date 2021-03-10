d802cb4bab1ee1595c4d3259ef928fb3cf12a404
Julian Tallar <jtallar@itba.edu.ar>
2021-03-10 14:19:03 -0300

    Fix map initialization bug

--- main.py
+++ main.py
@@ -22,8 +22,7 @@ for line in file:
     x = 0
     for character in line:
         if character == '\n':
-            continue
-        x += 1
+            break
         if character == '#':
             static_map[obj.Point(x, y)] = mapFun.Element.Wall
         elif character == '.':
@@ -38,8 +37,8 @@ for line in file:
         elif character == '+':
             static_map[obj.Point(x, y)] = mapFun.Element.Goal
             player_init = obj.Point(x, y)
-        elif character == ' ':
-            continue
+        # elif character == ' ': do nothing
+        x += 1
     y += 1
 
 # TODO: Create simple map validation?
